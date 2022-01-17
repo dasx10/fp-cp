cf8eeb6bd0449437e4b8e5d4c3b0cb278be43687
dasx10 <dasx10@gmail.com>
2022-01-17 16:39:45 +0200

    feat: 🎸 new F

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const groupBy = require("./array/groupBy");
 
-/**
- * @param {string} e 
- * @returns 
- */
-const getLength = (e) => e.length;
-const groupByLength = groupBy(getLength);
-const a = groupByLength(['test', 'ddd', 'dsw', 'dd', 'dsadsa'])
-console.log(a);
+const arr = new Array(33550000);
+arr.fill(100);
+
+const map = (f, a) => {
+    const { length } = a;
+    const newArr = new Array(length);
+    let i = 0;
+    while (i < length) {
+        newArr[i] = f(a[i], i, a);
+        // i++;
+        i = -~i;
+    }
+    return newArr;
+}
+
+// const sum = (a) => a ^ 3;
+const sum = (a) => Math.imul(a, a);
+
+console.time('-');
+map(sum, arr)
+console.timeEnd('-');
