c2dd7797c8611b60891dfc33b8f27b8808a8ba9c
dasx10 <dasx10@gmail.com>
2022-01-21 14:11:34 +0200

    feat: 🎸 where

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const averageBy = require("./array/averageBy");
+const partition = require("./array/partition");
+const where = require("./array/where");
+
+const aa = where({ a: (e) => e > 10, b: (e) => e < 10 })
 
 console.log(
-    averageBy((el) => el)([12, 10])
-    
+    aa([{a: 1, b: 2}, {a: 11, b: 9 }, { a: 11, b: 11 }])
 );
