505c9bbc2cc3e6e28e41eb5694a19b5eaa9fc93a
dasx10 <dasx10@gmail.com>
2022-02-08 19:21:25 +0200

    new f

--- src/string/char/at/index.D.ts
+++ src/string/char/at/index.D.ts
@@ -1,3 +1,5 @@
+import { Chars, CharsReverse } from "../../chars/index.D";
+
 export type CharOf<Value extends string> = string extends Value
   ? string
   : Value extends `${infer Char}${infer NextChars}`
@@ -5,3 +7,10 @@ export type CharOf<Value extends string> = string extends Value
       ? Char
       : Char | CharOf<NextChars>
     : Value;
+
+export type CharAt<Value extends string, Index extends number = number> = string extends Value
+    ? string | ''
+    : `${Index}` extends `-${infer Reverse}`
+        // @ts-ignore
+        ? ['', ...CharsReverse<Value>][Reverse] extends undefined ? '' : ['', ...CharsReverse<Value>][Reverse]
+        : Chars<Value>[Index] extends undefined ? '' : Chars<Value>[Index];
