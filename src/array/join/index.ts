9aed0c83f406ec8e486f51733ad5b27f3fabca2c
dasx10 <dasx10@gmail.com>
2022-02-15 19:44:22 +0200

    join

--- src/array/join/index.ts
+++ src/array/join/index.ts
@@ -1,13 +1,17 @@
 function join (separator = ', ') {
     return function useJoin (array: string[]) {
         const { length } = array;
-        if (length) {
-            let result = '' + array[0];
-            let index = 1;
+        switch (length) {
+          case 0: return '';
+          case 1: return array[0];
+          default: {
+            let result = array[0] + separator + array[1];
+            let index = 2;
             while (index < length) {
                 result += array[index] + separator;
                 index++;
             }
+          }
         }
         return '';
     }
