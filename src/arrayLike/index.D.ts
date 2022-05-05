1ebb501620ef814e2379ed292332502ab9e00d7b
dasx10 <dasx10@gmail.com>
2022-05-05 12:03:58 +0300

    feat: 🎸 new v

--- src/arrayLike/index.D.ts
+++ src/arrayLike/index.D.ts
@@ -1,13 +1,12 @@
-import type { ArrayLikeAtLib } from './at/index.D';
 import type { ToIndexRight } from './index/index.D';
 
 export type ArrayLikeValue <X extends ArrayLike<any>> = X extends ArrayLike<infer T> ? T : unknown;
 
-export type ArrayLikeIterator <X, R = any> = (value: X, index: ToIndexRight<number>, arrayLike: ArrayLike<X>) => R;
+export type ArrayLikeIterator <X, Return = any> = (value: X, index: ToIndexRight<number>, arrayLike: ArrayLike<X>) => Return;
 
-export type ArrayLikeEachDef<R, X = any, RX = any> = <IX extends X>(def: ArrayLikeIterator<IX, RX>, x: ArrayLike<X>) => R;
+export type ArrayLikeEachDef<Result, X = any, Return = any> = <IX extends X>(def: ArrayLikeIterator<IX, Return>, x: ArrayLike<X>) => Result;
 
-export type ArrayLikeEachCurryDef<R, RX = any> = {
-  <X>(def: ArrayLikeIterator<X, RX>, x: ArrayLike<X>): R,
-  <X>(def: ArrayLikeIterator<X, RX>): (x: ArrayLike<X>) => R
+export type ArrayLikeEachCurryDef<Result, Return = any> = {
+  <X>(def: ArrayLikeIterator<X, Return>, x: ArrayLike<X>): Result;
+  <X>(def: ArrayLikeIterator<X, Return>): (x: ArrayLike<X>) => Result;
 }
