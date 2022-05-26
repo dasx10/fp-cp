cf0ca9bff336f308a1dfac235225e36a4ecd3109
dasx10 <dasx10@gmail.com>
2022-05-27 01:33:17 +0300

    feat: 🎸 new algoritme reduce

--- src/arrayLike/reduce/core/index.ts
+++ src/arrayLike/reduce/core/index.ts
@@ -2,23 +2,24 @@ import type { ArrayLikeValue } from "../../index.D";
 import type { ArrayLikeReduceCore } from "./index.D";
 
 const arrayLikeReduceCore: ArrayLikeReduceCore = <INITIAL_VALUE, X extends ArrayLike<unknown>>(
-  initialValue : INITIAL_VALUE,
   def          : (currentValue: INITIAL_VALUE, Value: ArrayLikeValue<X>, index: number, x: X) => INITIAL_VALUE,
+  initialValue : INITIAL_VALUE,
   x            : X
 ) => {
   const { length } = x;
-  if (length > 0) {
-    let currentValue = initialValue;
-    let index = 0;
-    while (index < length) {
-      currentValue = def(currentValue, x[index], index, x);
-      index++;
-    }
-
-    return currentValue;
-  }
-
-  return initialValue;
+	switch (length) {
+		case 0  : return initialValue;
+		case 1  : return def(initialValue, x[0], 0, x);
+		default : {
+			let currentValue = def(initialValue, x[0], 0, x);
+			let index = 1;
+			while (index < length) {
+				currentValue = def(currentValue, x[index], index, x);
+				index++;
+			}
+			return currentValue;
+		}
+	}
 }
 
 export default arrayLikeReduceCore;
