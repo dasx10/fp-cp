c966183e0bbda184c63785889d6097cbad42e617
dasx10 <dasx10@gmail.com>
2022-07-01 01:22:20 +0300

    new odds

---
+++ src/array/like/odds/contract/index.ts
@@ -0,0 +1,6 @@
+import _curry2           from "../../../../function/curry/2/_/index";
+import arrayLikeOddsCore from "../core/index";
+
+const arrayLikeOddsDef = _curry2(arrayLikeOddsCore);
+export default arrayLikeOddsDef;
+
